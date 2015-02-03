70493bdf2f8c37b1d110d96ffa78e4ffa98bfa71
emg5478 <emg5478@206-L54.up.ist.local>
2015-02-02 21:04:07 -0500

    Color Changing and Diffculty Slider

--- Shapes Game/src/shapesgame/MyJPanel.java
+++ Shapes Game/src/shapesgame/MyJPanel.java
@@ -6,13 +6,18 @@
 package shapesgame;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JPanel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  *
  * @author emg5478
  */
-public class MyJPanel extends JPanel {
+public class MyJPanel extends JPanel implements ActionListener, ChangeListener {
     
     MyJPanel1 mjp1;
     MyJPanel2 mjp2;
@@ -32,9 +37,44 @@ public class MyJPanel extends JPanel {
         this.add(mjp1, "North");
         
         this.add(mjp2, "South");
+        
+        mjp1.start.addActionListener(this);
+        
+        mjp2.speed.addChangeListener(this);
+        mjp2.color.addActionListener(this);
+        
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        Object obj = e.getSource();
+        if (obj == mjp1.start) {
             
+            mjp2.changeColor();
+        }
+         if (obj == mjp2.color) {
+            
+           mjp2.changeColor();
+            
+        }
         
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
         
+        Object obj = e.getSource();
+        
+        if (obj == mjp2.speed && !mjp2.speed.getValueIsAdjusting()) {
+            
+           int newSpeed = mjp2.speed.getValue();
+           
+           mjp2.setSpeed(newSpeed);
+            
+        }
+    
     }
     
 }
--- Shapes Game/src/shapesgame/MyJPanel2.java
+++ Shapes Game/src/shapesgame/MyJPanel2.java
@@ -5,6 +5,7 @@
  */
 package shapesgame;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Hashtable;
@@ -12,12 +13,14 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  *
  * @author emg5478
  */
-public class MyJPanel2 extends JPanel implements ActionListener {
+public class MyJPanel2 extends JPanel {
 
     JSlider speed;
     JButton color;
@@ -26,7 +29,7 @@ public class MyJPanel2 extends JPanel implements ActionListener {
     public MyJPanel2() {
         
         super();
-        speed = new JSlider(JSlider.HORIZONTAL, 0, 2, 1);
+        speed = new JSlider(JSlider.HORIZONTAL, 1, 3, 2);
         
         color = new JButton("Color");
         
@@ -34,9 +37,9 @@ public class MyJPanel2 extends JPanel implements ActionListener {
         
         //Create the label table
         Hashtable labelTable = new Hashtable();
-        labelTable.put(0, new JLabel("Slowest") );
-        labelTable.put(1, new JLabel("Medium") );
-        labelTable.put(2, new JLabel("Fast") );
+        labelTable.put(1, new JLabel("Slowest") );
+        labelTable.put(2, new JLabel("Medium") );
+        labelTable.put(3, new JLabel("Fast") );
         
         speed.setLabelTable(labelTable);
         speed.setMinorTickSpacing(2);
@@ -51,18 +54,47 @@ public class MyJPanel2 extends JPanel implements ActionListener {
     
     }
     
-    
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    public void setSpeed(int s) {
 
-       Object obj = e.getSource();
+        speed.setValue(s);
     
-       if (obj == speed) {
-           
-           MyJPanel3 mjp3;
-    
-           
-       }
     }
+    
+    public void changeColor() {
+        
+        int i = (int) (10 * Math.random());
+        
+        Color c = Color.red;
+        
+        switch(i) {
+            
+            case 0: c = Color.BLACK;
+                break;
+            case 1: c = Color.BLUE;
+                break;
+            case 2: c = Color.CYAN;
+                break;
+            case 3: c = Color.DARK_GRAY;
+                break;
+            case 4: c = Color.GREEN;
+                break;
+            case 5: c = Color.MAGENTA;
+                break;
+            case 6: c = Color.ORANGE;
+                break;
+            case 7: c = Color.PINK;
+                break;
+            case 8: c = Color.RED;
+                break;
+            case 9: c = Color.YELLOW;
+                break;
+            case 10: c = Color.LIGHT_GRAY;
+                break;
+            
+        }
+        
+        color.setBackground(c);
+    }
+
     
 }
