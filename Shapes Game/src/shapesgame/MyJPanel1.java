5f05a6020fbfc7357d84c7196c652ef91786053d
fsy5001 <fsy5001@206-L53.up.ist.local>
2015-02-02 19:41:47 -0500

    Working on JPanel 1

--- Shapes Game/src/shapesgame/MyJPanel1.java
+++ Shapes Game/src/shapesgame/MyJPanel1.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package shapesgame;
-
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 /**
  *
  * @author emg5478
  */
-public class MyJPanel1 {
+public class MyJPanel1 extends JPanel implements ActionListener {
+    
+    public MyJPanel1 () {
+        
+        JButton start = new JButton ();
+        start.addActionListener(this);  
+        add(start);
+        JButton exit = new JButton ();
+        exit.addActionListener(this);
+        add(exit);
+    }
+    public void actionPerformed(ActionEvent colors){
+        
+    }
     
 }
